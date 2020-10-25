9ceb2ac403d253e45929d930f5af0645fce80dcf
sm <sorooshm78@gmail.com>
2020-10-25 19:19:02 +0330

    add command GET in source and add comment

--- source/Main.cpp
+++ source/Main.cpp
@@ -5,22 +5,34 @@ using namespace std;
 
 int main()
 {
-cout<<"hi soroosh"<<endl;
-redisReply *reply;
-redisContext *c = redisConnect("127.0.0.1", 6379);
-if (c == NULL or c->err) {
-    if (c) {
-        cout<<"Error";
+	redisReply *reply;
+	redisContext *c = redisConnect("127.0.0.1", 6379);
+	//check connect or not
+	if (c == NULL or c->err) {
+    	if (c) {
+        	cout<<"Error";
         
-    } else {
-        cout<<"Can't allocate redis context\n";
-    }
+   		 } else {
+        	cout<<"Can't allocate redis context\n";
+    	}
     
-}
-redisCommand(c, "SET num 12");
-redisCommand(c, "SET age 21");
-//reply = redisCommand(c,"GET age"); 
-//cout <<"GET :"<<reply->str<<endl;
-cout <<"bye soroosh"<<endl;
+	}
+	//send redis command and set name and age
+	redisCommand(c, "SET name soroosh");
+	redisCommand(c, "SET age 21");
+	//get name from redis
+	reply = (redisReply *) redisCommand(c,"GET name"); 
+	cout <<"name :"<<reply->str<<endl;
+	//get age from redis
+	reply = (redisReply *) redisCommand(c,"GET age");
+	cout <<"age :"<<reply->str<<endl;
+	//shutdown and nosave from redis
+    redisCommand(c, "SHUTDOWN NOSAVE");
+    //quit from redis
+    redisCommand(c, "QUIT");
+	//free reply
+	freeReplyObject(reply);
+	//disconnect
+	redisFree(c);
 }
 
