4f4d0f531215f7de8b8f6d12a1864572db682a0b
sm <sorooshm78@gmail.com>
2020-10-29 12:32:40 +0330

    solve key and value by space

--- source/Main.cpp
+++ source/Main.cpp
@@ -9,10 +9,26 @@ int main()
 {
 	cout << "-----------" << endl;
 	somredis s("127.0.0.1", 6379);
+	cout << "key and value normal" << endl;
 	s.insert("fname","mehran");
 	cout << "SET fname mehran" << endl;
 	cout << "get fname: " << s.get("fname") << endl;
     cout << "-----------" << endl;
+	cout << "key normal and value by space" << endl;
+	s.insert("lname","sed soroosh");
+	cout << "SET lname sed soroosh" << endl;
+	cout << "get lname: " << s.get("lname") << endl;
+	cout << "-----------" << endl;
+	cout << "key by space and value normal" << endl;
+	s.insert("first name","ahmad");
+	cout << "SET first name = ahmad" << endl;
+	cout << "get first name: " << s.get("first name") << endl;
+	cout << "-----------" << endl;
+	cout << "key and value by space" << endl;
+	s.insert("last name","allah gholi");
+	cout << "SET last name = allah gholi" << endl;
+	cout << "get last name: " << s.get("last name") << endl;
+	cout << "-----------" << endl;
 	cout << "size befor clear: " << s.size() << endl;
 	cout << "empty: " << s.empty() << endl;
 	cout << "-----------" << endl;
