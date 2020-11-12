f98856d6492bad9b08f0c35f2253f7f410dac625
sm <sorooshm78@gmail.com>
2020-11-12 22:50:15 +0330

    add function getopt for performane

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,11 @@
-#include <string>
-#include <vector>
-#include <iostream>
-#include <stdlib.h> 
-#include <time.h> 
 #include <hiredis/hiredis.h>
+#include <iostream>
 
 #include "SomRedis.h"
 
-using namespace std;
-
-string random(int byte)
-{
-	int count_byte = byte;
-	char k[count_byte];
-	srand(time(0));
-	for(int i = 0 ; i < count_byte ; i++)
-	{
-		k[i] = rand() % 256;
-	}
-	return string(k);
-}
-
-int main()
-{
-	somredis s("127.0.0.1", 6379);
-	string key = random(30);
-	cout << key.size() << endl;
-	s.insert(key, "sina");
-}
-
+using namespace std; 
+ 
+int main(int argc, char *argv[])  
+{ 
+   return 0; 
+} 
--- performance/performance.cpp
+++ performance/performance.cpp
@@ -1,7 +1,9 @@
+#include <iostream>
+#include <unistd.h>
+#include <stdio.h>
+#include <vector>
 #include <string>
 #include <chrono>
-#include <iostream>
-#include <vector>
 
 #include <hiredis/hiredis.h>
 
@@ -13,43 +15,118 @@ using namespace std::chrono;
 string str_rand(int byte)
 {
 	int count_byte = byte;
-	char k[count_byte];
+	char str[count_byte];
 	for(int i = 0 ; i < count_byte ; i++)
 	{
-		k[i] = rand() % 256;
+		str[i] = rand() % 256;
 	}
-	return string(k);
+	return string(str);
 }
 
-int main()
+void performance(somredis &s, string type, int count, int key_byte, int val_byte)
+{
+	vector <string> keys;
+	if(type == "set" or type == "sg")
+	{
+		// Computing set performance
+		auto start_set = high_resolution_clock::now();
+		for (int i = 0 ; i < count ; i++)
+		{	
+			keys.push_back(str_rand(key_byte));
+			s.insert(keys[i], str_rand(val_byte));
+		}
+		auto end_set = high_resolution_clock::now();
+		auto durtion_set = duration_cast<chrono::microseconds>(end_set - start_set).count();
+		double time_s = (double)durtion_set / (double) 1000000;
+		cout << "performance set : " << (int)(count / time_s)  << " per/sec" << endl;
+	}
+
+	if(type == "get" or type == "sg")
+	{
+		// Computing get performance
+		auto start_get = high_resolution_clock::now();
+		for (int i = 0 ; i < count ; i++)
+			s.get(keys[i]);
+		
+		auto end_get = high_resolution_clock::now();
+		auto durtion_get = duration_cast<chrono::microseconds>(end_get - start_get).count();
+		double time_g = (double)durtion_get / (double) 1000000;
+		cout << "performance get : " << (int)(count / time_g)  << " per/sec" << endl;
+	}
+}
+
+void man_page()
+{
+	cout << "this is man page:" <<endl;
+	cout << "-h  help " << endl;
+	cout << "-k  size byte key default: 30 " << endl; 
+	cout << "-v  size byte value default: 100 " << endl;
+	cout << "-t  type of performance set or get or sg(set and get) default: set " << endl;
+	cout << "-c  ip or unix default: ip " << endl;
+	cout << "-n  count set or get to redis default: 100 " << endl;
+}
+
+int main(int argc, char *argv[])
 {	
-	// Configure
-	int count = 5000;
+	// Configure default
+	int opt;
+	int count = 100;
 	int key_byte = 30;
 	int val_byte = 100;
-	vector <string> keys;
+	string type = "set";
+	string connect = "ip";
+
+	// Srand for random function
 	srand(time(0));
-	somredis s("127.0.0.1", 6379);
-
-	// Computing set performance
-	auto start_set = high_resolution_clock::now();
-	for (int i = 0 ; i < count ; i++)
+	
+	// Getopt for send argument
+    while((opt = getopt(argc, argv, ":c:t:n:k:v:h")) != -1)
 	{
-		keys.push_back(str_rand(key_byte));
-		s.insert(keys[i], str_rand(val_byte));
-	}
-	auto end_set = high_resolution_clock::now();
-	auto durtion_set = duration_cast<chrono::microseconds>(end_set - start_set).count();
-	double time_s = (double)durtion_set / (double) 1000000;
-	cout << "performance set : " << (int)(count / time_s)  << " per/sec" << endl;
+		switch(opt)
+		{			
+			case 'h':
+			man_page();
+			return 0;
+	
+			case 'k':
+			key_byte = stoi(optarg);
+			break;
+			
+			case 'v':
+			val_byte = stoi(optarg);	
+			break;
 
-	// Computing get performance
-	auto start_get = high_resolution_clock::now();
-	for (int i = 0 ; i < count ; i++)
-		s.get(keys[i]);
-		
-	auto end_get = high_resolution_clock::now();
-	auto durtion_get = duration_cast<chrono::microseconds>(end_get - start_get).count();
-	double time_g = (double)durtion_get / (double) 1000000;
-	cout << "performance get : " << (int)(count / time_g)  << " per/sec" << endl;
+			case 'n':
+			count = stoi(optarg); 
+			break;
+
+			case 't':
+			type = optarg;
+			break;
+			
+			case 'c':
+			connect = optarg;
+			break;			
+
+			case ':':
+			cout << "option need value" << endl;
+			break;
+
+			case '?':
+			cout << "unknown option: %c" << optopt << endl; 
+			break;
+		}
+	}
+
+	if(connect == "ip")
+	{
+		somredis s("127.0.0.1", 6379);
+		performance(s, type, count, key_byte, val_byte);
+	}
+
+	if(connect == "unix")
+	{
+		somredis s("/var/run/redis/redis.sock");
+		performance(s, type, count, key_byte, val_byte);
+	}
 }
